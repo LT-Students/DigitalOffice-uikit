0735beecdda32333b1c24c4ef12c33231bac77d3
NGurev <nickita.gurev@yandex.ru>
2021-04-29 14:32:08 +0300

    added mat form field styling in case of project
    -added styling for all states
    -added styling for input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatFormFieldDefaultOptions } from '@angular/material/form-field';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +10,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'do-uikit';
 
-  formFieldAppearance = 'outline';
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  matFormFieldDefaultOptions: MatFormFieldDefaultOptions = { appearance: 'outline'};
 
   seasons: string[] = ['Winter', 'Spring', 'Summer', 'Autumn'];
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MaterialModule
-  ],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MaterialModule,
+		ReactiveFormsModule
+	],
   providers: [],
   bootstrap: [AppComponent],
 })
