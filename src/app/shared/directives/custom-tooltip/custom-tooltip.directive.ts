cfd961a2832b9db8a7312a32a24c6b5de564865c
romantsinevich <braid98@yandex.ru>
2021-04-29 15:26:52 +0300

    Revert "Some refactoring"
    This reverts commit 76901ba4540681114a832735278490ced6bc4eeb.
    
    # Conflicts:
    #	src/app/shared/components/custom-tooltip/custom-tooltip/custom-tooltip.component.scss
    #	src/app/shared/directives/custom-tooltip/custom-tooltip.directive.ts

---
+++ src/app/shared/directives/custom-tooltip/custom-tooltip.directive.ts
@@ -0,0 +1,73 @@
+import {
+  ComponentRef,
+  Directive,
+  ElementRef,
+  HostListener,
+  Input,
+  OnInit,
+} from '@angular/core';
+import {
+  Overlay,
+  OverlayPositionBuilder,
+  OverlayRef,
+} from '@angular/cdk/overlay';
+import { ComponentPortal } from '@angular/cdk/portal';
+import { CustomTooltipComponent } from '../../components/custom-tooltip/custom-tooltip/custom-tooltip.component';
+
+@Directive({
+  selector: '[appCustomTooltip]',
+})
+export class CustomTooltipDirective implements OnInit {
+  @Input() appCustomTooltip: string;
+
+  private _overlayRef: OverlayRef;
+
+  constructor(
+    private _overlay: Overlay,
+    private _overlayPositionBuilder: OverlayPositionBuilder,
+    private _elementRef: ElementRef,
+  ) {
+  }
+
+  ngOnInit() {
+    const positionStrategy = this._overlayPositionBuilder
+      .flexibleConnectedTo(this._elementRef)
+      .withPositions([
+        {
+          originX: 'end',
+          originY: 'top',
+          overlayX: 'end',
+          overlayY: 'bottom',
+        },
+        {
+          originX: 'end',
+          originY: 'top',
+          overlayX: 'start',
+          overlayY: 'bottom',
+        }
+      ]);
+
+    this._overlayRef = this._overlay.create({positionStrategy});
+  }
+
+  @HostListener('mouseenter')
+  show() {
+    if (this._overlayRef && !this._overlayRef.hasAttached() && this.appCustomTooltip) {
+      const tooltipRef: ComponentRef<CustomTooltipComponent> = this._overlayRef.attach(
+        new ComponentPortal(CustomTooltipComponent),
+      );
+      tooltipRef.instance.text = this.appCustomTooltip;
+    }
+  }
+
+  @HostListener('mouseleave')
+  hide() {
+    this.closeToolTip();
+  }
+
+  private closeToolTip() {
+    if (this._overlayRef) {
+      this._overlayRef.detach();
+    }
+  }
+}
